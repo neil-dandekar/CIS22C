cbf010b1a15014609410c6021d02315a9791c67f
Neil Dandekar <neil.dandekar@icloud.com>
2023-03-04 14:58:24 -0800

    beginning of lab 4

---
+++ lab4/BSTNode.cpp
@@ -0,0 +1,7 @@
+// NAME BLOCK
+// Lab:  04
+// Name: Neil Dandekar & Xi Gao
+// The purpose of lab 04 is to demonstrate Trees (Binary Search Trees) and 
+// its algorithms in OOP programming.
+///////////////////////////////////////////////////////////////////////////
+
---
+++ lab4/Queue.cpp
@@ -0,0 +1,60 @@
+// NAME BLOCK
+// Lab:  03
+// Name: Neil Dandekar & Xi Gao
+// The purpose of lab 03 is to demonstrate the List (LinkedList), Stack, 
+// and Queue ADTs in OOP programming.
+///////////////////////////////////////////////////////////////////////////
+
+#include "Currency.cpp"
+#include "SinglyLinkedList.cpp"
+
+class Queue: private SinglyLinkedList {
+    public:
+    // CONSTRUCTORS
+    Queue(): SinglyLinkedList() {}
+    Queue(Currency* curr): SinglyLinkedList(curr) {}
+
+    // DESTRUCTOR
+    ~Queue() {}
+
+    // INSTANCE METHODS
+    void enqueue(Currency* curr) {
+    // PURPOSE:  Adds Currency object to the queue.
+    // PRE:    - Method must be called using class instance.
+    // POST:   - The Currency object is added to the queue.
+    // RETURN: - Nothing is returned.
+        SinglyLinkedList::addCurrency(curr, countCurrency());
+    }
+
+    Currency* dequeue() {
+    // PURPOSE:  Removes Currency object from the queue.
+    // PRE:    - Method must be called using class instance.
+    // POST:   - The Currency object is removed from the queue.
+    // RETURN: - The removed Currency is returned.
+        return SinglyLinkedList::removeCurrency(0);
+    }
+
+    Currency* peekFront() {
+    // PURPOSE:  Returns the Currency at the front of the queue.
+    // PRE:    - Method must be called using class instance.
+    // POST:   - Nothing is changed.
+    // RETURN: - The front Currency object is returned.
+        return SinglyLinkedList::getCurrency(0);
+    }
+
+    Currency* peekRear() {
+    // PURPOSE:  Returns the Currency at the rear of the queue.
+    // PRE:    - Method must be called using class instance.
+    // POST:   - Nothing is changed.
+    // RETURN: - The rear Currency object is returned.
+        return SinglyLinkedList::getCurrency(countCurrency()-1);
+    }
+
+    void printQueue() {
+    // PURPOSE:  Prints the queue.
+    // PRE:    - Method must be called using class instance.
+    // POST:   - The queue contents are printed.
+    // RETURN: - Nothing is returned.
+        SinglyLinkedList::printList();
+    }
+};
