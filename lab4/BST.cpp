c97361532472969499dd430a7ae651cfc24a6b70
Neil Dandekar <neildandekar10@gmail.com>
2023-03-06 23:59:18 -0800

    clearn up

--- lab4/BST.cpp
+++ lab4/BST.cpp
@@ -423,33 +423,3 @@ class BST {
         return root == nullptr;
     }
 };
-
-// int main() {
-//     // Temp array of Krone values
-//     double arr[20] =
-//         {57.12, 23.44, 87.43, 68.99, 111.22, 44.55,
-//         77.77, 18.36, 543.21, 20.21, 345.67, 36.18,
-//         48.48, 101.00, 11.00, 21.00, 51.00, 1.00,
-//         251.00, 151.00};
-//     // Create Krone array
-//     Krone* kr[20];
-//     for(int i = 0; i < 20; i++) {
-//         kr[i] = new Krone(arr[i]);
-//     }
-//     ofstream myFile;
-//     myFile.open("output.txt");
-//     // BST tree
-//     BST tree(myFile);
-//     for(int i = 0; i < 20; i++) {
-//         tree.insert(tree.getRoot(), kr[i]);
-//     }
-
-//     tree.print();
-    
-//     printf("\nSTART DELETE");
-//     tree.deleteKr(new Krone(20.21));
-//     printf("\nEND DELETE\n");
-
-//     printf("\n");
-//     tree.print();
-// }
